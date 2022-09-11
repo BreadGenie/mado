c73c515221897438fdefa0f5473be2f427163d08
Bread Genie <msuhailbh07@gmail.com>
2022-09-11 14:33:45 +0530

    chore: rename to mado

--- client/src/App.js
+++ client/src/App.js
@@ -39,7 +39,7 @@ const App = () => {
     <div className={classes.wrapper}>
       <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography variant="h2" align="center">
-          창문
+          Mado (窓)
         </Typography>
       </AppBar>
       <VideoPlayer />
