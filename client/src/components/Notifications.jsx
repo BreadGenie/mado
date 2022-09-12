94f7b2de1dc2a7159b3097e2ec485b78f230b9a2
Bread Genie <msuhailbh07@gmail.com>
2022-09-12 22:12:14 +0530

    refactor: call notification to modal

--- client/src/components/Notifications.jsx
+++ client/src/components/Notifications.jsx
@@ -1,35 +1,65 @@
 import React, { useContext } from "react";
-import { Button } from "@material-ui/core";
+import { Button, Modal, Box } from "@material-ui/core";
 import { Call, CallEnd } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core/styles";
 
 import { SocketContext } from "../SocketContext";
 
+const useStyles = makeStyles((theme) => ({
+  box: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    backgroundColor: "#ffffff99",
+    border: "2px solid #000",
+    boxShadow: 24,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+  buttonContainer: {
+    padding: "10px",
+  },
+}));
+
 const Notifications = () => {
+  const classes = useStyles();
+
   const { call, answerCall, callAccepted, declineCall } =
     useContext(SocketContext);
 
   return (
     <>
       {call.isRecievedCall && !callAccepted && (
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <h1>{call.name} is calling</h1>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={answerCall}
-            startIcon={<Call />}
-          >
-            Answer
-          </Button>
-          <Button
-            variant="contained"
-            color="secondary"
-            onClick={declineCall}
-            startIcon={<CallEnd />}
-          >
-            Decline
-          </Button>
-        </div>
+        <Modal
+          open={call.isRecievedCall && !callAccepted}
+          onClose={declineCall}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box className={classes.box}>
+            <h1>{call.name} is calling</h1>
+            <Box className={classes.buttonContainer}>
+              <Button
+                style={{ margin: 10 }}
+                variant="contained"
+                color="primary"
+                onClick={answerCall}
+                startIcon={<Call />}
+              />
+              <Button
+                style={{ margin: 10 }}
+                variant="contained"
+                color="secondary"
+                onClick={declineCall}
+                startIcon={<CallEnd />}
+              />
+            </Box>
+          </Box>
+        </Modal>
       )}
     </>
   );
