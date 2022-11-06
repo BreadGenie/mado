706ca5bb37940ac58597e7481ccdceb3fb60daa1
Bread Genie <msuhailbh07@gmail.com>
2022-11-06 10:17:18 +0530

    refactor: migrate from CRA to Vite

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -2,7 +2,7 @@ import io from "socket.io-client";
 import { Peer } from "peerjs";
 
 export const socket = io(
-  process.env.REACT_APP_SERVER_URL ?? "http://localhost:5000/"
+  import.meta.env.REACT_APP_SERVER_URL ?? "http://localhost:5000/"
 );
 
 export const peer = new Peer();
